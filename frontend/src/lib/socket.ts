bd2fecd3035bc4cabfe54e02ce8d43062ca3767a
Aquiles69x4 <xavir9123@gmail.com>
2025-09-19 10:42:01 -0500

    Link nuevo sockets

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -9,7 +9,7 @@ class SocketClient {
   connect() {
     if (this.socket) return this.socket
 
-    this.socket = io(process.env.NEXT_PUBLIC_API_URL?.replace('/api', '') || 'http://localhost:3004')
+    this.socket = io(process.env.NEXT_PUBLIC_API_URL?.replace('/api', '') || 'https://backendotravez.vercel.app/')
 
     this.socket.on('connect', () => {
       console.log('Connected to server')
@@ -58,5 +58,6 @@ class SocketClient {
     }
   }
 }
+
 
 export const socketClient = new SocketClient()
